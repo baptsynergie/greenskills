4b16f9dc1578e9342b53eded05ea2597daa2ef77
baptsynergie <baptiste.gauthier@synergiefamily.com>
2024-06-11 17:20:31 +0200

    debut inte formulaire + instal de svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import db from "@astrojs/db";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), db()]
+  integrations: [tailwind(), db(), svelte()]
 });

c08d162fececfe2a6e9955de102bcb2a7f8e1ad1
baptsynergie <baptiste.gauthier@synergiefamily.com>
2024-06-12 13:02:15 +0200

    finition inte

--- src/components/StepForm.svelte
+++ src/components/StepForm.svelte
@@ -1,15 +1,18 @@
 <script>
 
     import { onMount } from "svelte";
+    import { Image } from 'astro:assets';
+    import alveoles from '../images/pictos/alveole_tkpage.svg';
 
     let circles;
+    let step = 1;
 
     onMount(() => {
         circles = document.querySelectorAll('.step-circle');
         // Do something with nd, such as adding event listeners, styles, etc.
     });
 
-    function handleClick(event) {
+    function changeColorCircle(event) {
         let target = event.target;
         if(!target.classList.contains("active")) {
             target.classList.add("active");
@@ -19,6 +22,10 @@
                 }
             });
         }
+    }
+
+    function changeStep(number) {
+        step = number;
     }
 
 </script>
@@ -34,103 +41,116 @@
                 <h2 class="text-white text-3xl font-finalSixBlack">Contactez-nous</h2>
             </div>
             <div class="flex items-center">
-                <button class="border-white border-solid border-[2px] w-[30px] h-[30px] rounded-full active cursor-pointer active step-circle" on:click={handleClick}></button>
+                <button  class={`border-white border-solid border-[2px] w-[30px] h-[30px] rounded-full cursor-pointer step-circle ${step === 1 ? 'active' : ''}`} on:click|preventDefault={(e) => { changeColorCircle(e); changeStep(1); }}></button>
                 <div class="bg-white w-[60px] h-[2px]"></div>
-                <button class="border-white border-solid border-[2px] w-[30px] h-[30px] rounded-full cursor-pointer step-circle" on:click={handleClick}></button>
+                <button class={`border-white border-solid border-[2px] w-[30px] h-[30px] rounded-full cursor-pointer step-circle ${step === 2 ? 'active' : ''}`} on:click|preventDefault={(e) => { changeColorCircle(e); changeStep(2); }}></button>
                 <div class="bg-white w-[60px] h-[2px]"></div>
-                <button class="border-white border-solid border-[2px] w-[30px] h-[30px] rounded-full cursor-pointer step-circle" on:click={handleClick}></button>
+                <button class={`border-white border-solid border-[2px] w-[30px] h-[30px] rounded-full cursor-pointer step-circle ${step === 3 ? 'active' : ''}`} on:click|preventDefault={(e) => { changeColorCircle(e); changeStep(3); }}></button>
             </div>
         </div>
         
 
-        <form class="shadow-lg bg-white lg:p-24 p-10 rounded-xl">
+        <form class="shadow-lg bg-white lg:p-24 p-10 rounded-xl relative">
 
-            <div id="step_one">
+            {#if step === 1} 
+                <div id="step_one">
 
-                <h1 class=" font-finalSixMedium text-lg text-center mb-12">À la recherche d’un nouveau défi professionnel ?</h1>
+                    <h1 class=" font-finalSixMedium text-lg text-center mb-12">À la recherche d’un nouveau défi professionnel ?</h1>
 
-                <div class="lg:flex lg:items-center lg:gap-5 mb-7">
-                    <div class="lg:w-1/2 mb-7 lg:mb-0">
-                        <label class="hidden" for="lastname">Nom :</label>
-                        <input placeholder="Nom*" type="text" name="lastname" class="bg-[#F3F3F3] text-[#9E9E9E] font-finalSixMedium outline-green outline-2 py-3 px-8 rounded-lg w-full" />
+                    <div class="lg:flex lg:items-center lg:gap-5 mb-7">
+                        <div class="lg:w-1/2 mb-7 lg:mb-0">
+                            <label class="hidden" for="lastname">Nom :</label>
+                            <input placeholder="Nom*" type="text" name="lastname" class="bg-[#F3F3F3] text-[#9E9E9E] font-finalSixMedium outline-green outline-2 py-3 px-8 rounded-lg w-full" />
+                        </div>
+            
+                        <div class="lg:w-1/2 mb-7 lg:mb-0">
+                            <label class="hidden" for="firstname">Prénom :</label>
+                            <input placeholder="Prénom*"type="text" name="firstname" class="bg-[#F3F3F3] text-[#9E9E9E] font-finalSixMedium outline-green outline-2 py-3 px-8 rounded-lg w-full" />
+                        </div>
                     </div>
-        
-                    <div class="lg:w-1/2 mb-7 lg:mb-0">
-                        <label class="hidden" for="firstname">Prénom :</label>
-                        <input placeholder="Prénom*"type="text" name="firstname" class="bg-[#F3F3F3] text-[#9E9E9E] font-finalSixMedium outline-green outline-2 py-3 px-8 rounded-lg w-full" />
+
+                    <div class="lg:flex lg:items-center lg:gap-5 mb-7">
+                        <div class="lg:w-1/2 mb-7 lg:mb-0">
+                            <label class="hidden" for="email">Email :</label>
+                            <input placeholder="Email*" type="email" name="email" class="bg-[#F3F3F3] text-[#9E9E9E] font-finalSixMedium outline-green outline-2 py-3 px-8 rounded-lg w-full" />
+                        </div>
+            
+                        <div class="lg:w-1/2 mb-7 lg:mb-0">
+                            <label class="hidden" for="phone">Téléphone :</label>
+                            <input placeholder="Téléphone*" type="tel" name="phone" class="bg-[#F3F3F3] text-[#9E9E9E] font-finalSixMedium outline-green outline-2 py-3 px-8 rounded-lg w-full" />
+                        </div>
+                    </div>
+
+                    <div class="lg:w-1/2">
+                        <label class="hidden" for="city">Ville :</label>
+                        <input placeholder="Ville*" type="text" name="city" class="bg-[#F3F3F3] text-[#9E9E9E] font-finalSixMedium outline-green outline-2 py-3 px-8 rounded-lg w-full" />
+                    </div>
+                    
+                    <div class="lg:flex lg:justify-between lg:items-center mt-7 lg:mt-0">
+                        <p class="text-[#9E9E9E] font-finalSixMedium">Champs obligatoires(*)</p>
+                        <button class="js-next-btn mt-7 font-finalSixMedium bg-green text-white rounded-lg py-3 px-8" on:click|preventDefault={() => { changeStep(2); }}>Continuer</button>
+                    </div>
+
+                </div>
+            {/if}
+
+            {#if step === 2} 
+                <div id="step_two">
+
+                    <h1 class=" font-finalSixMedium text-lg text-center mb-12">Laissez-nous en savoir un peu plus...</h1>
+
+                    <div class="lg:w-full mb-7">
+                        <label class="hidden" for="situation">Je suis </label>
+                        <select name="situation" class="bg-[#F3F3F3] text-[#9E9E9E] font-finalSixMedium outline-green outline-2 py-3 px-8 rounded-lg w-full">
+                            <option value="">Je suis...</option>
+                                <option value="A la recherche d’un premier travail">A la recherche d’un premier travail</option>
+                                <option value="En reconversion professionnelle et intéressé.e par les métiers verts.">En reconversion professionnelle et intéressé.e par les métiers verts.</option>
+                                <option value="Déjà dans le secteur mais je suis actuellement sans emploi.">Déjà dans le secteur mais je suis actuellement sans emploi.</option>
+                                <option value="Peu au courant et je souhaite me sensibiliser sur les métiers verts de l’industrie pour savoir si cela peut m’intéresser.">Peu au courant et je souhaite me sensibiliser sur les métiers verts de l’industrie pour savoir si cela peut m’intéresser.</option>
+                        </select>
+                    </div>
+                    <div class="lg:w-full mb-7">
+                        <label class="hidden" for="school_level">Je suis </label>
+                        <select name="school_level" class="bg-[#F3F3F3] text-[#9E9E9E] font-finalSixMedium outline-green outline-2 py-3 px-8 rounded-lg w-full">
+                            <option value="">Quel est votre niveau d'études ?</option>
+                                <option value="Infra Bac">Infra Bac</option>
+                                <option value="Bac professionnel">Bac professionnel</option>
+                                <option value="Bac +2 +3 ou équivalent">Bac +2 +3 ou équivalent</option>
+                                <option value="Bac +5 ou doctorant.e">Bac +5 ou doctorant.e</option>
+                        </select>
+                    </div>
+                    <div class="lg:w-full mb-7">
+                        <label class="hidden" for="how_know">Je suis </label>
+                        <select name="how_know" class="bg-[#F3F3F3] text-[#9E9E9E] font-finalSixMedium outline-green outline-2 py-3 px-8 rounded-lg w-full">
+                            <option value="">Comment avez-vous entendu parler de notre programme ?</option>
+                                <option value="France Travail">France Travail</option>
+                                <option value="APEC">APEC</option>
+                                <option value="Missions locales">Missions locales</option>
+                                <option value="Réseau sociaux et internet">Réseau sociaux et internet</option>
+                                <option value="Réseau sociaux et internet">Réseau sociaux et internet</option>
+                                <option value="Bouche à oreille">Bouche à oreille</option>
+                                <option value="Autres">Autres</option>
+                        </select>
+                    </div>
+
+                    <div class="lg:flex lg:justify-between lg:items-center mt-7 lg:mt-0">
+                        <div>
+                            <input type="checkbox" id="cgv" name="scales" required />
+                             <label class="font-finalSixMedium text-sm" for="cgv">En envoyant ce formulaire, j’accepte que la team Green Skills me recontacte.</label>
+                        </div>
+                        <button class="mt-7 font-finalSixMedium bg-green text-white rounded-lg py-3 px-8" on:click|preventDefault={(e) => { changeColorCircle(e); changeStep(3); }}>Valider le formulaire</button>
                     </div>
                 </div>
+            {/if}
 
-                <div class="lg:flex lg:items-center lg:gap-5 mb-7">
-                    <div class="lg:w-1/2 mb-7 lg:mb-0">
-                        <label class="hidden" for="email">Email :</label>
-                        <input placeholder="Email*" type="email" name="email" class="bg-[#F3F3F3] text-[#9E9E9E] font-finalSixMedium outline-green outline-2 py-3 px-8 rounded-lg w-full" />
-                    </div>
-        
-                    <div class="lg:w-1/2 mb-7 lg:mb-0">
-                        <label class="hidden" for="phone">Téléphone :</label>
-                        <input placeholder="Téléphone*" type="tel" name="phone" class="bg-[#F3F3F3] text-[#9E9E9E] font-finalSixMedium outline-green outline-2 py-3 px-8 rounded-lg w-full" />
-                    </div>
-                </div>
+            {#if step === 3} 
+                <div id="step_tkyou">
 
-                <div class="lg:w-1/2">
-                    <label class="hidden" for="city">Ville :</label>
-                    <input placeholder="Ville*" type="text" name="city" class="bg-[#F3F3F3] text-[#9E9E9E] font-finalSixMedium outline-green outline-2 py-3 px-8 rounded-lg w-full" />
+                    <img alt="greens skills synergie family" src="src/images/pictos/alveole_tkpage.svg" class="absolute top-0 right-0" />
+                    <div class="text-green uppercase font-finalSixHeavy text-8xl text-center">Merci !</div>
+                    <div class=" font-finalSixMedium text-black text-lg text-center">Notre équipe vous recontactera dès que possible</div>
                 </div>
-                
-                <div class="lg:flex lg:justify-between lg:items-center mt-7 lg:mt-0">
-                    <p class="text-[#9E9E9E] font-finalSixMedium">Champs obligatoires(*)</p>
-                    <button class="mt-7 font-finalSixMedium bg-green text-white rounded-lg py-3 px-8">Continuer</button>
-                </div>
-
-            </div>
-
-            <div id="step_two">
-
-                <h1 class=" font-finalSixMedium text-lg text-center mb-12">Laissez-nous en savoir un peu plus...</h1>
-
-                <div class="lg:w-1/2 mb-7">
-                    <label class="hidden" for="situation">Je suis </label>
-                    <select name="situation" class="bg-[#F3F3F3] text-[#9E9E9E] font-finalSixMedium outline-green outline-2 py-3 px-8 rounded-lg w-full">
-                        <option value="">Je suis...</option>
-                            <option value="A la recherche d’un premier travail">A la recherche d’un premier travail</option>
-                            <option value="En reconversion professionnelle et intéressé.e par les métiers verts.">En reconversion professionnelle et intéressé.e par les métiers verts.</option>
-                            <option value="Déjà dans le secteur mais je suis actuellement sans emploi.">Déjà dans le secteur mais je suis actuellement sans emploi.</option>
-                            <option value="Peu au courant et je souhaite me sensibiliser sur les métiers verts de l’industrie pour savoir si cela peut m’intéresser.">Peu au courant et je souhaite me sensibiliser sur les métiers verts de l’industrie pour savoir si cela peut m’intéresser.</option>
-                    </select>
-                </div>
-                <div class="lg:w-1/2 mb-7">
-                    <label class="hidden" for="school_level">Je suis </label>
-                    <select name="school_level" class="bg-[#F3F3F3] text-[#9E9E9E] font-finalSixMedium outline-green outline-2 py-3 px-8 rounded-lg w-full">
-                        <option value="">Quel est votre niveau d'études ?</option>
-                            <option value="Infra Bac">Infra Bac</option>
-                            <option value="Bac professionnel">Bac professionnel</option>
-                            <option value="Bac +2 +3 ou équivalent">Bac +2 +3 ou équivalent</option>
-                            <option value="Bac +5 ou doctorant.e">Bac +5 ou doctorant.e</option>
-                    </select>
-                </div>
-                <div class="lg:w-1/2 mb-7">
-                    <label class="hidden" for="how_know">Je suis </label>
-                    <select name="how_know" class="bg-[#F3F3F3] text-[#9E9E9E] font-finalSixMedium outline-green outline-2 py-3 px-8 rounded-lg w-full">
-                        <option value="">Comment avez-vous entendu parler de notre programme ?</option>
-                            <option value="France Travail">France Travail</option>
-                            <option value="APEC">APEC</option>
-                            <option value="Missions locales">Missions locales</option>
-                            <option value="Réseau sociaux et internet">Réseau sociaux et internet</option>
-                            <option value="Réseau sociaux et internet">Réseau sociaux et internet</option>
-                            <option value="Bouche à oreille">Bouche à oreille</option>
-                            <option value="Autres">Autres</option>
-                    </select>
-                </div>
-
-                <div class="lg:flex lg:justify-between lg:items-center mt-7 lg:mt-0">
-                    <div>
-                        <input type="checkbox" id="cgv" name="scales" required />
-                        <label class="font-finalSixLight italic" for="cgv">En envoyant ce formulaire, j’accepte que la team Green Skills me recontacte.</label>
-                    </div>
-                    <button class="mt-7 font-finalSixMedium bg-green text-white rounded-lg py-3 px-8">Valider le formulaire</button>
-                </div>
-            </div>
+            {/if}
             
         </form>
     </div>
